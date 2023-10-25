1148c2799fefcd267244016f2838e1c880393324
nikhilgit02 <srivastavanikhil107@gmail.com>
2023-10-25 19:06:49 +0530

    Added about and interest components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Info from './components/Info';
+import About from './components/About';
+import Interest from './components/Interest';
 
 
 function App() {
   return (
+    <div>
     <Info/>
-  );
+    <About/>
+    <Interest/>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Interest.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Interest(){
+    return(
+        <div className="info">
+            <div className="about">
+                <h1>Interest</h1>
+                <p>Food expert. Music scholar. Reader. 
+                    Internet fanatic. Bacon buff. Entrepreneur. 
+                    Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>
+        </div>
+    )
+}
